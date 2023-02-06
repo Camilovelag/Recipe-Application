b24182e7aef88e8acfef7648e46ec5d500258615
Camilovelag <camilo.vela.galindo@gmail.com>
2023-02-06 16:42:37 -0500

    Setup devise

--- Gemfile
+++ Gemfile
@@ -76,3 +76,4 @@ end
 
 gem 'dotenv-rails', '~> 2.8'
 gem 'rubocop', '>= 1.0', '< 2.0'
+gem 'devise'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable
   has_many :recipes
   has_many :foods
 end
