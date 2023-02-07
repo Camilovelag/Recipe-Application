7207f854f7387f1766083ed486224f2073960112
Camilovelag <camilo.vela.galindo@gmail.com>
2023-02-07 13:01:33 -0500

    Add recipe_show action/view

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,6 +3,10 @@ class RecipesController < ApplicationController
     @recipes = Recipe.all
   end
 
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
   def new
     @recipe = Recipe.new
   end
