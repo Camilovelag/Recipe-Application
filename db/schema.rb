78a27bba2953b4fef83b6d4b0ade89ffecdd4db2
Camilovelag <camilo.vela.galindo@gmail.com>
2023-02-10 17:29:02 -0500

    Add changes

--- db/schema.rb
+++ db/schema.rb
@@ -72,11 +72,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_09_022417) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
