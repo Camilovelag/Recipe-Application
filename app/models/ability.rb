7b87d4778dbc80bab6d2095bab07afb6fa7c3dc4
Camilovelag <camilo.vela.galindo@gmail.com>
2023-02-09 20:53:33 -0500

    Update ability.rb

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,11 +1,12 @@
-# frozen_string_literal: true
-
 class Ability
   include CanCan::Ability
 
   def initialize(user)
+    can :read, Recipe, public: true
+
     return unless user.present?
-    can :manage, RecipeFood, user: user
-    can :manage, Recipe, user: user
+
+    can(:manage, RecipeFood, user:)
+    can :manage, Recipe, user:
   end
 end
